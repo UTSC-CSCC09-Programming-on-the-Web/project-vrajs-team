de7fc7a2aaa92a4d01ff6645f443fb827b8cf52e
HardikNarkhede <hardikn2010@gmail.com>
2025-06-26 00:38:11 -0400

    Completed initial development of quiz master form, with basic validation for form elements and styling

--- openquizzer/client/frontend/src/main.ts
+++ openquizzer/client/frontend/src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { Masterform } from './app/components/quizform/masterform/masterform';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
