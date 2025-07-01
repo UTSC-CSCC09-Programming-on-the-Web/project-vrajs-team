99212d3440a987431e427bdb7ed4be2510469c5d
Arhum Parvez <arhum.parvez@mail.utoronto.ca>
2025-07-01 04:59:11 -0400

    Wrote queries to validate and insert a user into our Users table for implementing login/signup backend

--- openquizzer/server/models/userModel.js
+++ openquizzer/server/models/userModel.js
@@ -1,1 +1,24 @@
 //pg queries of User table to interact with db.
+
+const database = require("../config/db");
+//DB query to check whether the user exists or not 
+exports.userExists = async(userName) =>  { 
+  const { rows } = await database.query(
+    `SELECT 1
+    FROM "Users"
+    WHERE username=$1`,
+    [userName]
+  );
+  if(rows[0])
+    return true;
+  return false;
+}
+
+//DB query to input user into our Users table
+exports.insertUser = async(userName,hashedPassword)=>  { 
+  await database.query(
+    `INSERT INTO "Users" (username,password_hash)
+    VALUES ($1,$2)`,
+    [userName,hashedPassword]
+  );
+}
